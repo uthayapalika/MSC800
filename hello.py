cb8b72a4ecaadb3c435152470b5db016cee71403
uthayapalika <uthayapalika@gmail.com>
2026-08-02 10:19:31 +1200

    requirement file added

--- hello.py
+++ hello.py
@@ -4,27 +4,6 @@ b=20;
 c=a+b;
 print("The sum of a and b is:", c)
 
-# Function to print Fibonacci series
-def fibonacci(n):
-    a, b = 0, 1
-    while a <= n:
-        print(a, end=" ")
-        a, b = b, a + b
+print([0,1]+[sum([0,1][i:i+2]) for i in range(8)])
+import math; print(math.factorial(int(input("N: "))))
 
-# Function to calculate factorial
-def factorial(n):
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
-
-# Main function
-def main():
-    N = int(input("Enter a number: "))
-
-    print("Fibonacci series up to", N, ":")
-    fibonacci(N)
-
-    print("\nFactorial of", N, "is:", factorial(N))
-
-main()
