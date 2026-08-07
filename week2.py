a1fcf7700e673b0cfaece58d79ca6d7df058ad34
uthayapalika <uthayapalika@gmail.com>
2026-08-08 09:26:48 +1200

    comment added

--- week2.py
+++ week2.py
@@ -2,12 +2,15 @@ class Bmi:
     def calculate_bmi(self, weight, height):
         return weight / (height ** 2)
 
+#define class instance
 bmi_calculator = Bmi()   
-
+#user input BMI
 weight = float(input("Enter weight in kg: "))
 height = float(input("Enter height in meters: "))
+#call the class specific function
 bmi = bmi_calculator.calculate_bmi(weight, height)
 #print("The calculated BMI is:", bmi)
+
 
 if bmi < 18.5:
     print("You are underweight.")
