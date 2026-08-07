5e9dcf1409867208d22f201e7f0581eda6b47507
uthayapalika <uthayapalika@gmail.com>
2026-08-08 09:22:27 +1200

    added if else

--- week2.py
+++ week2.py
@@ -7,4 +7,13 @@ bmi_calculator = Bmi()
 weight = float(input("Enter weight in kg: "))
 height = float(input("Enter height in meters: "))
 bmi = bmi_calculator.calculate_bmi(weight, height)
-print("The calculated BMI is:", bmi)
+#print("The calculated BMI is:", bmi)
+
+if bmi < 18.5:
+    print("You are underweight.")
+elif 18.5 <= bmi < 25:
+    print("You have a normal weight.")
+elif 25 <= bmi < 30:
+    print("You are overweight.")
+else:
+    print("You are obese.")
