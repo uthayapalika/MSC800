9ccb002657c0705e9ac29db115c88b4ea24fa9a8
uthayapalika <uthayapalika@gmail.com>
2026-08-09 09:51:15 +1200

    activity

--- class_activity.py
+++ class_activity.py
@@ -1,7 +1,8 @@
 my_list = [1, 3, 5, 7, 9, 11]
 my_list[2:4] = [-3, -9, -11, -13]
-print(my_list)
+#print(my_list)
+
 
 my_list = [1, 3, 5, 7, 9, 11]
-my_list[2:6] = [-3, -9, -11, -13,-7, -5, -1]
+my_list[2:2] = [-3, -9, -11, -13, -7, -5]
 print(my_list)
