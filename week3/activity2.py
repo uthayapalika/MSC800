e2fdb4a69d122d63e7df0209dee1344311e16a7c
uthayapalika <uthayapalika@gmail.com>
2026-08-15 10:06:51 +1200

    week 3 activity 2

---
+++ week3/activity2.py
@@ -0,0 +1,15 @@
+from pyparsing import line
+
+
+data = open(r"C:\Users\uthay\OneDrive\Documents\MSC800\week3\junk.txt")
+lines = data.readlines() # Get a list of all the lines in the file
+print(lines) # Print the list of lines
+#print total number of lines in the file
+print(f'Total number of lines in the file: {len(lines)}')
+#total number of different flowers available 
+different_flowers = set(line.split(',')[0] for line in lines)
+print(f'Total number of different flowers in the dataset: {len(different_flowers)}')
+#total different flowers in the dataset
+flower_names = set(line.split(',')[0] for line in lines)
+print(f'Total number of different flowers in the dataset: {len(flower_names)}')
+data.close()
